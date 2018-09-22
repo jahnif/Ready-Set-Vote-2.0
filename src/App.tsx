5d1818e7c436137329fb48dbb27faa299e645bbf
Will Kuhn <emailpublicwill@gmail.com>
2018-09-22 12:52:44 -0700

    Simple image logos building

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import './App.css';
+import CallToAction from './CallToAction';
+import Footer from './Footer';
 
+import './css/ml.css';
 import logo from './logo.svg';
 
 class App extends React.Component {
@@ -9,11 +12,13 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React </h1>
         </header>
+          <CallToAction />
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Footer />
       </div>
     );
   }
