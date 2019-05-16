eb89e3a120ee818140f620cfcd1f10890dbd6dc5
vinhub <vin@introp.net>
2019-05-15 18:11:24 -0700

    address page: address validation and error display getEmail page: email address validation and error display

--- src/address/Address.tsx
+++ src/address/Address.tsx
@@ -6,8 +6,20 @@ import rsvLogo from "../img/rsvLogo-new.svg";
 import warningImg from '../img/warning.svg';
 import { addressStore } from "./AddressStore";
 
-class Address extends React.Component<RouteComponentProps> {
-  private addressStore = addressStore;
+interface IAddressState {
+  address: string;
+  addressError: boolean;
+}
+
+class Address extends React.Component<RouteComponentProps, IAddressState> {
+  constructor(props: any){
+    super(props);
+
+    this.state = {
+      address: addressStore.address,
+      addressError: false,
+    };
+  }
 
   public render() {
     return (
@@ -20,8 +32,8 @@ class Address extends React.Component<RouteComponentProps> {
           <div className="addressBox" style={{display: "block"}}>
             <p>Enter your street address and we'll recreate your ballot.</p>
             <form>
-              <input type="text" className="myAddress" id="address" placeholder="e.g., 220 2nd Ave S, Seattle WA" value={this.addressStore.address}/>
-              <div className="errAddressBox">
+              <input type="text" className="myAddress" id="address" placeholder="e.g., 220 2nd Ave S, Seattle WA" value={this.state.address} onChange={this.handleChange}/>
+              <div className={ this.state.addressError ? "" : "errAddressBox" }>
                 <div id="errAddress">
                   <img src={warningImg} width="30" height="30" style={{display: "inline-block", margin: "0em 1em 0em 0em"}} />
                   Invalid street address.
@@ -39,11 +51,25 @@ class Address extends React.Component<RouteComponentProps> {
       </div>
     );
   }
+
+  private handleChange = (e: any) => {         
+    this.setState({ address: e.target.value });
+  };
     
-  private onSubmit = () => {
-    // Submit info to API and on success redirect to next page
-    this.props.history.push(`/ballot`);
-  }
+  private onSubmit = (e: any) => {
+    if (this.state.address && this.validateAddress(this.state.address)) {
+      addressStore.setAddress(this.state.address);
+      this.props.history.push(`/ballot`);
+    } else {
+      this.setState({ addressError: true });
+      e.preventDefault();
+    }
+  };
+
+  private validateAddress(address: string) { 
+    // TODO: validate address using API?
+    return (address.replace(/\s/g, "").length > 0);
+  } 
 }
 
 export default Address;
