cf5217c232049b93ced813e2308b1e4915237b57
Leilani Leach <leilanileach@yahoo.com>
2019-07-27 13:16:28 -0700

    start admin editing component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Address from './address/Address';
-import Admin from './admin/Admin'
+import Admin from './admin/Admin';
+import EditTable from './admin/EditTable'
+import MyAdmin from './admin/MyAdmin';
 import './App.css';
 import Ballot from './ballot/Ballot';
 import "./css/ml.css";
@@ -20,7 +22,9 @@ class App extends React.Component {
           <Route path="/ballot" component={Ballot} />
           <Route path="/email" component={GetEmail} />
           <Route path="/guide" component={Guide} />
+          <Route path="/myadmin" component={MyAdmin} />
           <Route path="/admin" component={Admin} />
+          <Route path="/admin/*" component={EditTable} />
         </div>
       </Router>
     );
---
+++ src/admin/EditTable.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+
+const tableStyles = {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '75%'
+}
+class EditTable extends React.Component {
+    public render() {
+        return (
+            <table style={tableStyles}>
+                <tr>
+                    <th>Img</th>
+                    <th>Candidate</th>
+                    <th>Seat</th>
+                    <th>Edit</th>
+                </tr>
+                <tr>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>Test</td>
+                </tr>
+                <tr>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>Test</td>
+                </tr>
+                <tr>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>Test</td>
+                </tr>
+            </table>
+        );
+    }
+}
+
+export default EditTable;
